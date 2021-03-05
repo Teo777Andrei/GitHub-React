a41517746f14c682d5bb47d5afd92a7f6587ce9f
Teo777Andrei <luputeo777@yahoo.com>
2021-03-05 22:43:29 +0200

    Styled Warning component

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -3,11 +3,14 @@ import React from "react"
 
 function Warning({text}){
     
-    return(
+    return (
         <div className = "card card-body mt-5">
-            <h4 className = "quicksand">{text}</h4>
+            <div id ="warning" className ="text-center">
+                <h5 className ="quicksand">No user found</h5>
+            </div>
         </div>
     )
+    
 }
 
 
